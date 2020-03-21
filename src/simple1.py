aa4ce2ed55aaa6c71c27e95703623e523789981e
Tom Tarpey <tomtarpeydev@gmail.com>
2020-03-21 17:09:16 +0000

    testing how i might make an oop computer simulator

--- src/simple1.py
+++ src/simple1.py
@@ -1,10 +1,26 @@
 # let's make a simple data driven machine!!!
 
 class CPU:
-    pass
+    def __init__(self):
+        self.pc = 0
+        self.sp = -1
+        self.registers = []
+        self.flags = []
+        self.cache = []
+
+
 
 class Memory:
-    pass
+    def __init__(self, mem_size):
+        self.storage = [0] * mem_size
+
+    def read(self, addr):
+        if addr < len(self.storage):
+            return self.storage[addr]
+
+    def write(self, addr, value):
+        if addr < len(self.storage):
+            self.storage[addr] = value
 
 class Machine:
     pass
