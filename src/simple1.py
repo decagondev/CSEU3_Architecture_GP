b7e2651b643326f3dbb21bb806b59dfd1713c415
Tom Tarpey <tomtarpeydev@gmail.com>
2020-03-21 19:15:57 +0000

    added some structure to day 1 simple machine code [comments]"

--- src/simple1.py
+++ src/simple1.py
@@ -1,4 +1,18 @@
 # let's make a simple data driven machine!!!
 
+
 # think of some operations that we might want to perform such as print something, load  or store something etc
 # maybe some way to stop execution and some arithmetic operations
+
+# lets make a model of memory to hold our program
+
+# think about keeping track where we are currently in mem to fetch the next instruction
+
+# are we actually running
+
+# lets make a running loop...
+    # extract a command maybe?
+
+    # lets check what command has been fetched
+
+    # how will we move forward in memory to grab the next command?
