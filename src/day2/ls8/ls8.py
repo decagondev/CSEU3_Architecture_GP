88ad9191b0afd524394a8a1ed55a74e7b40fd1d0
Tom Tarpey <tomtarpeydev@gmail.com>
2020-03-24 13:12:13 +0000

    refactored load method

---
+++ src/day2/ls8/ls8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+if len(sys.argv) != 2:
+    print("usage: simple.py filename", file=sys.stderr)
+    sys.exit(1)
+
+
+cpu = CPU()
+
+cpu.load(sys.argv[1])
+cpu.run()
